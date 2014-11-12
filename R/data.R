d8633172d24fd216849d4cf0c57cd9afc014e762
hadley <h.wickham@gmail.com>
2014-11-12 14:14:51 -0600

    Add example to snp500.

--- R/data.R
+++ R/data.R
@@ -1,4 +1,9 @@
 #' SNP 500 data
 #'
 #' The Daily Standard & Poor index from March 25, 1999 to March 9, 2007.
+#'
+#' @examples
+#' plot(snp500, type = "l")
+#' lines(l1tf(snp500, lambda = 50), col = "red")
+#' lines(l1tf(snp500, prop = 0.01), col = "blue")
 "snp500"

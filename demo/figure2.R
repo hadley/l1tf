eeba5bcbce5b82ea8e92b74638f56228ea82a7b7
Dirk Eddelbuettel <edd@debian.org>
2014-11-12 20:24:32 -0600

    matching par(op)

--- demo/figure2.R
+++ demo/figure2.R
@@ -3,8 +3,8 @@ library(l1tf)
 
 op <- par(mfrow=c(2,1), mar=c(3,4,1,1))
 with(sp500, plot(date, log(sp500), type='l', ylab="log-price"))
-
 with(sp500, {
     plot(date, log(sp500), type="l", lty="dotted", col="mediumblue", ylab="log-price")
     lines(date, l1tf(log(sp500), lambda=100), col="blue")
 })
+par(op)
